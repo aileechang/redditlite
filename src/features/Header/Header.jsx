d22693312d6fe25f6654a7f153364c6300140518
aileechang <aileechang.swe@gmail.com>
2024-08-12 10:56:25 -0800

    Add header

---
+++ src/features/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import redditliteLogo from '../../assets/redditlite-logo.png';
+import { FaSearch, FaHome } from "react-icons/fa";
+import './Header.css';
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <div className='logo'>
+                    <img src={redditliteLogo} alt="RedditLite Logo" className='logoIcon'/>
+                    <p>reddit<span>Lite</span></p>
+                </div>
+                <form className='search' >
+                    <input
+                        type='text'
+                        placeholder='Search Reddit'
+                    />
+                    <FaSearch alt="Search" className="searchIcon" />
+                </form>
+                <div className='.home'>
+                    <a href="/" className="home-link">
+                        <FaHome alt="Home" className="homeIcon" />
+                    </a>
+                </div>
+            </header>
+        </>
+    );
+};
+
+export default Header;
