897a61a51ffa45b87d5af104e8b557f803aa37d0
aileechang <aileechang.swe@gmail.com>
2024-08-13 00:19:32 -0800

    Feat: Add search functionality in Header

--- src/features/Header/Header.jsx
+++ src/features/Header/Header.jsx
@@ -1,9 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setSearchTerm } from '../../store/redditSlice';
 import redditliteLogo from '../../assets/redditlite-logo.png';
 import { FaSearch, FaHome } from "react-icons/fa";
 import './Header.css';
 
 const Header = () => {
+    const [localSearchTerm, setLocalSearchTerm] = useState('');
+    const searchTerm = useSelector((state) => state.reddit.searchTerm);
+    const dispatch = useDispatch();
+
+    const onSearchChange = (e) => {
+        setLocalSearchTerm(e.target.value);
+    };
+
+    useEffect(() => {
+        setLocalSearchTerm(searchTerm);
+    }, [searchTerm]);
+
+    const onSearchSubmit = (e) => {
+        e.preventDefault();
+        dispatch(setSearchTerm(localSearchTerm));
+    };
+
     return (
         <>
             <header>
@@ -11,10 +30,13 @@ const Header = () => {
                     <img src={redditliteLogo} alt="RedditLite Logo" className='logoIcon'/>
                     <p>reddit<span>Lite</span></p>
                 </div>
-                <form className='search' >
+                <form className='search' onSubmit={onSearchSubmit}>
                     <input
                         type='text'
                         placeholder='Search Reddit'
+                        value={localSearchTerm}
+                        onChange={onSearchChange}
+                        aria-label='Search reddit posts'
                     />
                     <FaSearch alt="Search" className="searchIcon" />
                 </form>
